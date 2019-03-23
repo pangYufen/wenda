664da7963992acfc47ba6befee4a5083e7647903
pangYufen <2560420025@qq.com>
2019-03-23 16:18:56 +0800

    hello2

--- src/com/newcoder/test.java
+++ src/com/newcoder/test.java
@@ -3,5 +3,6 @@ package com.newcoder;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello java");
     }
 }
